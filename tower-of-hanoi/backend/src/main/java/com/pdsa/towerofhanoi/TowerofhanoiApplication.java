a725324b2ed694bb2ebd36d9bdbc9022727515a7
Marian <mariansenadheera@gmail.com>
2025-11-12 13:17:10 +0530

    Add Tower of Hanoi backend: Spring Boot setup with entity classes and MySQL configuration

---
+++ tower-of-hanoi/backend/src/main/java/com/pdsa/towerofhanoi/TowerofhanoiApplication.java
@@ -0,0 +1,13 @@
+package com.pdsa.towerofhanoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerofhanoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TowerofhanoiApplication.class, args);
+	}
+
+}
---
+++ tower-of-hanoi/backend/src/test/java/com/pdsa/towerofhanoi/TowerofhanoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdsa.towerofhanoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowerofhanoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
