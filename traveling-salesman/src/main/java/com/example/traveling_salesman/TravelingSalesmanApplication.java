14445b964616e67030e334b1f71e853e3ea9480c
Your <your_email@example.com>
2025-11-16 14:53:59 +0530

    init:Spring boot and MySQL

---
+++ traveling-salesman/src/main/java/com/example/traveling_salesman/TravelingSalesmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.traveling_salesman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingSalesmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingSalesmanApplication.class, args);
+	}
+
+}
---
+++ traveling-salesman/src/test/java/com/example/traveling_salesman/TravelingSalesmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traveling_salesman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingSalesmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
