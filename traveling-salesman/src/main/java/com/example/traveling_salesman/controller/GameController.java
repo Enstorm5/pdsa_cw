a1acfcd7776e9609a105cf20ced72c32b4e60afd
Your <your_email@example.com>
2025-12-09 21:11:14 +0530

    feat: add CrossOrigin

--- traveling-salesman/src/main/java/com/example/traveling_salesman/controller/GameController.java
+++ traveling-salesman/src/main/java/com/example/traveling_salesman/controller/GameController.java
@@ -8,6 +8,7 @@ import com.example.traveling_salesman.dto.StartGameRequest;
 import com.example.traveling_salesman.service.GameService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8089")
 @RequestMapping("/api/game")
 public class GameController {
 
